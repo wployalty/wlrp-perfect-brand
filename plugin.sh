12e6c3e0f09a32d944a66e63250816c4d99af6fc
sabhari-krr <sabhari.a.krr@gmail.com>
2025-02-18 18:42:27 +0530

    fix: Quote variables in plugin.sh to prevent potential issues with spaces

--- plugin.sh
+++ plugin.sh
@@ -7,8 +7,8 @@ composer_lock_path=$current_dir"/composer.lock"
 vendor_path=$current_dir"/vendor"
 
 composer_run(){
-  rm $composer_lock_path
-  rm -r $vendor_path
+  rm "$composer_lock_path"
+  rm -r "$vendor_path"
   # shellcheck disable=SC2164
   cd "$current_dir"
   composer install --no-dev
